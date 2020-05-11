eb0a097b50929837215322f193e23885a20cad15
unknown <skopin.n@gmail.com>
2020-05-11 10:08:59 +0200

    Update and delete methods

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -52,21 +52,33 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  // const id = +req.params.id;
-  // const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  // const id = +req.params.id;
-  // const tour = tours.find((el) => el.id === id);
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+  } catch (error) {
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  }
 };
