ee64c595dd2a1e5f28fc024e9967688f2277a8e1
unknown <skopin.n@gmail.com>
2020-05-31 17:48:40 +0200

    Handling uncaught exeptions.

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXEPTION. Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
@@ -29,8 +35,8 @@ const server = app.listen(port, () => {
 
 //handle unhandled rejections
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION. Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
