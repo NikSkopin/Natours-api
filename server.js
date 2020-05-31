5d7923a65efdcf69eacda9390d2ff88d206ab121
unknown <skopin.n@gmail.com>
2020-05-31 17:39:10 +0200

    Handling unhandled rejections.

--- server.js
+++ server.js
@@ -23,6 +23,15 @@ const app = require('./app');
 //start server
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
+});
+
+//handle unhandled rejections
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION. Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
