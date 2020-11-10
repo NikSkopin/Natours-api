7df181098ed9e1497c682660cc93c622f9382183
unknown <skopin.n@gmail.com>
2020-11-10 21:01:46 +0100

    Get reviews and create review routs.

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+const {
+  getAllReviews,
+  createReview,
+} = require('../controllers/reviewController');
+
+const { protect, restrictTo } = require('../controllers/authController');
+
+router
+  .route('/')
+  .get(getAllReviews)
+  .post(protect, restrictTo('user'), createReview);
+
+module.exports = router;
